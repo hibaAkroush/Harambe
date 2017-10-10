7e05341b650de3122255abd845bc43e34028a538
hibaAkroush <hiba.akr@gmail.com>
2017-10-10 19:08:11 +0300

    recomended Movie feature add

--- public/components/rec.js
+++ public/components/rec.js
@@ -1,7 +1,13 @@
 angular.module('myapp')
 .component('rec',{
 	controller:function(){
+		this.title = this.recomendedMovie.title
+		this.poster = this.recomendedMovie.poster
+		this.date = this.recomendedMovie.date
 },
+	bindings:{
+		recomendedMovie:'<'
+	},
   templateUrl: 'public/templates/rec.html'
 
 });
